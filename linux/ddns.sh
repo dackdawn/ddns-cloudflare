8dddaf0dac973f7e9010057c57cd6f4ce80f0dac
dackdawn <25314430+dackdawn@users.noreply.github.com>
2019-02-13 16:57:31 +0800

    Update ddns.sh

--- linux/ddns.sh
+++ linux/ddns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip_ipv4=172.25.0.108
-result=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "X-Auth-Email: example@example.com" -H "X-Auth-Key: $key" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$domain\",\"content\":\"$ip_ipv4\",\"ttl\":120,\"proxied\":false}" -k)
+ip_ipv4=$(curl http://ipv4.ip.sb -k)
+result=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "X-Auth-Email: example@example.com" -H "X-Auth-Key: $key" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$domain\",\"content\":\"$ip_ipv4\",\"ttl\":120,\"proxied\":false}" -k -s)
 
 echo $result
