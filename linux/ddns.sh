2a6657ab6836144449bbd2ed7b596934822e60fa
dackdawn <25314430+dackdawn@users.noreply.github.com>
2019-02-23 16:46:40 +0800

    Update ddns.sh

--- linux/ddns.sh
+++ linux/ddns.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ip_ipv4=$(curl http://ipv4.ip.sb -k -s)
+#ip_ipv4=$(ifconfig | grep "172.25" | awk -F " " '{print $2}' | awk -F ":" '{print $2}')
 result=$(curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$id" -H "X-Auth-Email: example@example.com" -H "X-Auth-Key: $key" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$domain\",\"content\":\"$ip_ipv4\",\"ttl\":120,\"proxied\":false}" -k -s)
 
 echo $result
